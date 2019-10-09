5903ec7c2ba580d8c4e5619d2616a9f0ffc06297
maochichi <stefanmao1112@gmail.com>
2019-10-09 20:32:54 +0800

    linked list 應用

---
+++ 21. Merge Two Sorted Lists.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    
+    var result = new ListNode(0);
+    
+    var c = result;
+    
+    while(l1!==null && l2!==null)
+        {
+            if(l1.val>l2.val)
+                {
+                    c.next=l2;
+                    l2=l2.next;
+                }
+            else
+                {
+                    c.next=l1;
+                    l1=l1.next;
+                }
+            c=c.next;            
+        }
+   
+  
+    if(l1!==null)
+        {
+            c.next=l1;
+        }
+    
+    if(l2!==null)
+        {
+            c.next=l2;
+        }
+   
+    return result.next;
+    
+};
